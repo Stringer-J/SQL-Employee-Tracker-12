29f938e573b752cd427b9f9bea5de28ca74af591
Josh Stringer <joshstringer@live.com>
2024-08-01 11:26:20 -0500

    finished seed.sql and seeded database, sql queries in terminal work

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,21 @@
-insert into department (id, name) values
-('', ''),
-('', '');
+INSERT INTO department (id, name) values
+('1', 'Finance'),
+('2', 'Marketing'),
+('3', 'Facilities'),
+('4', 'Development'),
+('5', 'HR');
+
+INSERT INTO role (id, title, salary, department_id) values
+('1', 'Accountant', '50000', '1'),
+('2', 'Marketing Coordinator', '70000', '2'),
+('3', 'Maintenance', '40000', '3'),
+('4', 'Cloud Engineer', '60000', '4'),
+('5', 'Administrator', '80000', '5');
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) values
+('1', 'Bob', 'Ross', '1', '1'),
+('2', 'Clair', 'Redfield', '2', '2'),
+('3', 'Scott', 'Stapp', '3', '3'),
+('4', 'Steve', 'Urkel', '4', '4'),
+('5', 'Jill', 'Valentine', '5', '5');
