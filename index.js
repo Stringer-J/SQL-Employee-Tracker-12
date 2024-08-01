f890fb3cfb08b66065ab800ed257c72bdb9d2ac6
Josh Stringer <joshstringer@live.com>
2024-08-01 09:34:07 -0500

    added if statement to control response to selections

--- index.js
+++ index.js
@@ -15,4 +15,20 @@ inquirer
                       'update an employee role'
                      ]
         }
-    ])
+    ]).then((data) => {
+        if(data.intro === 'view all departments') {
+            console.log('1');
+        } else if(data.intro === 'view all roles') {
+            console.log('1');
+        } else if(data.intro === 'view all employees') {
+            console.log('1');
+        } else if(data.intro === 'add a department') {
+            console.log('1');
+        } else if(data.intro === 'add a role') {
+            console.log('1');
+        } else if(data.intro === 'add an employee') {
+            console.log('1');
+        } else if(data.intro === 'update an employee role') {
+            console.log('1');
+        } 
+    })
