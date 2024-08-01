8888914934f53fcbec8be286704943c476fa5b8d
Josh Stringer <joshstringer@live.com>
2024-08-01 14:11:38 -0500

    restructured index so that choices will be presented again after you select something and get a result, then added an exit choice so that you can end the cycle if you'd like

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const client = new Client({
     host: 'localhost',
     user: 'postgres',
-    password: 'Moogpualvo1!',
+    password: 'Moogpaulvo1!',
     database: 'employee_tracker'
 });
 
--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 const inquirer = require('inquirer');
+const { query } = require('./db');
 
-
-inquirer
-    .prompt([
+async function mainMenu() {
+    const data = await inquirer.prompt([
         {
             type: 'list',
             name: 'intro',
-            message: '\n\n=== Employee Tracker ===\nWhat would you like to do?',
+            message: 'What would you like to do?',
             choices: ['View All Departments',
                       'View All Roles',
                       'View All Employees',
                       'Add A Department',
                       'Add A Role',
                       'Add An Employee',
-                      'Update An Employee Role'
+                      'Update An Employee Role',
+                      'Exit'
                      ]
         }
-    ]).then((data) => {
+    ]);
+
+        //makes query when selecting view all departments
         if(data.intro === 'View All Departments') {
-            console.log('1');
+            //actual query
+            const result = await query('SELECT * FROM department');
+            console.table(result.rows);
+
+            //creates a formatted version of result, we are trying to get rid of index
+            const formatResult = result.rows.map(row => ({
+                ...row,
+            }));
+
+            //gets keys from object and runs formatted info into a new array
+            const headers = Object.keys(formatResult[0] || {});
+
+            //adds field names and dotted line
+            console.log(headers.join(' '));
+            // console.log(headers.map(() => '--').join('   '));
+
+            //iterates over each row in formatResult
+            formatResult.forEach(row => {
+                //puts array elements into same string with ' | ' separator and logs it to console
+                console.log(headers.map(header => row[header]).join('  '));
+            });
+
+        // makes query when selecting view all roles
         } else if(data.intro === 'View All Roles') {
-            console.log('1');
+        
+            const result = await query('SELECT * FROM role');
+            console.table(result.rows);
+            
         } else if(data.intro === 'View All Employees') {
             console.log('1');
         } else if(data.intro === 'Add A Department') {
@@ -31,5 +59,10 @@ inquirer
             console.log('1');
         } else if(data.intro === 'Update An Employee Role') {
             console.log('1');
-        } 
-    })
+        } else if(data.intro === 'Exit') {
+            process.exit(0);
+        }
+        await mainMenu();
+    }
+
+mainMenu();
