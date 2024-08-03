ca2a821a115b0f033ca3b8229285654f6332759f
Josh Stringer <joshstringer@live.com>
2024-08-03 11:49:42 -0500

    format function added to all necessary queries

--- index.js
+++ index.js
@@ -23,36 +23,22 @@ async function mainMenu() {
         if(data.intro === 'View All Departments') {
             //actual query
             const result = await query('SELECT * FROM department');
-            console.table(result.rows);
-
-            //creates a formatted version of result, we are trying to get rid of index
-            const formatResult = result.rows.map(row => ({
-                ...row,
-            }));
-
-            //gets keys from object and runs formatted info into a new array
-            const headers = Object.keys(formatResult[0] || {});
-
-            //adds field names and dotted line
-            console.log(headers.join(' '));
-            // console.log(headers.map(() => '--').join('   '));
-
-            //iterates over each row in formatResult
-            formatResult.forEach(row => {
-                //puts array elements into same string with ' | ' separator and logs it to console
-                // console.log(headers.map(header => row[header]).join('  '));
-            });
+            const formattedTable = formatTable(result.rows);
+            console.table(formattedTable);
 
         // makes query when selecting view all roles
         } else if(data.intro === 'View All Roles') {
         
             const result = await query('SELECT * FROM role');
-            console.table(result.rows);
+            const formattedTable = formatTable(result.rows);
+            console.table(formattedTable);
             
         } else if(data.intro === 'View All Employees') {
+            
             const result = await query('SELECT * FROM employee');
             const formattedTable = formatTable(result.rows);
             console.table(formattedTable);
+
         } else if(data.intro === 'Add A Department') {
             await addDept();
         } else if(data.intro === 'Add A Role') {
